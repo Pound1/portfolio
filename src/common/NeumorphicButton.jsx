1eaabacdd053e79cd4ffc7b4dd543aa42f2cf3e2
Pound1 <50385700+Pound1@users.noreply.github.com>
2025-09-09 21:04:59 +1000

    fixed Icons
    Updated colours; edited some of the content.

--- src/common/NeumorphicButton.jsx
+++ src/common/NeumorphicButton.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import styles from './NeumorphicButtonStyles.module.css';
-// import LinkedInIcon from '../assets/LinkedInIcon';
 
 const NeuButton = ({imageColour, Icon}) => {
     return (   
         <div className={styles.neumorphicButton} style={{color: imageColour}}>
             {/* <img src={LinkedInIcon} alt="icon" /> */}
             {/* <LinkedInIcon colour='purple' /> */}
-            {Icon && <Icon color='red'/>}
+            {Icon && <Icon />}
         </div>
      );
 }
  
 export default NeuButton;
-
-// get it to work with an image, then transform the image to a prompt, then pass the image
--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -1,32 +1,40 @@
 import ValueCard from '../../common/ValueCard';
 import styles from './HomeStyles.module.css'; 
-import leaf from '../../assets/leafIconSvg.svg';
-import lightbulbIconSvg from '../../assets/lightbulbIconSvg.svg';
+import lightbulbIcon from '../../assets/LightbulbIcon';
+import LeafIconSVG from '../../assets/LeafIconSVG';
+import PathIconSVG from '../../assets/PathIconSVG';
+
 
 function Home() {
   return (
     <section id="home" className={styles.content}>
         <h1>About me</h1>
-        <p>Hi! I'm Lachy. Step into my portfolio, where you can see my interests, history and love for technology. For example, building this website - from design [Figma], to code [React] to sharing [FileShare]. 
+        <p>Hey! I'm Lachy. Step into my portfolio, where you can see my interests, history and love for technology. For example, building this website - from design [Figma], to code [React] to sharing [FileShare]. 
 Be curious, broaden your horizons and you might find something you didn’t know.</p>
     <div className={styles.contactButton}>
         <a href="/contact">Contact</a>
     </div>
     <div className={styles.container}>
         <ValueCard 
-        img={leaf} 
-        title={"Vision"} 
-        description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}
+        Icon={LeafIconSVG}
+        // iconColour={"green"}
+        iconColour={"#BC572A"}
+        title={"Growth"} 
+        description={"Passion for improving - I have foundational growth mindset and mentor my team to do the same. Be it Tech skills; Self-awareness; or simply new hobbies - I love 'building on my base'."}
         />
         <ValueCard 
-        img={lightbulbIconSvg}
-        title={"Versatility"} 
+        Icon={lightbulbIcon}
+        title={"Energy"} 
         description={"Nam egestas lectus vel ante auctor dictum. Donec blandit, metus in dapibus aliquet, lorem lorem viverra ex, ac ullamcorper urna ante quis felis."}
+        // iconColour={"sandybrown"}
+        iconColour={"#BC572A"}
         />
         <ValueCard 
-        img={leaf} 
-        title={"Passion"} 
-        description={"Mauris at arcu iaculis, elementum ligula quis, volutpat velit. Etiam feugiat dolor sem, in porttitor elit ultrices ut."}
+        Icon={PathIconSVG}
+        // iconColour={"purple"} 
+        iconColour={"#BC572A"}
+        title={"Versatility"} 
+        description={"Jack of all trades doesn't quite tell the story: I've tackled many different roles - in life and work - and never afraid to fail."}
         />
     </div>
     </section>
