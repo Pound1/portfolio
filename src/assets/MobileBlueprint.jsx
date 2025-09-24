c886e38c2fa29371553befa6bfcbf67ffde0ab74
Pound1 <50385700+Pound1@users.noreply.github.com>
2025-09-24 16:09:57 +1000

    Added SVG
    plus general uplift

---
+++ src/assets/MobileBlueprint.jsx
@@ -0,0 +1,47 @@
+import styles from './MobileBlueprintStyles.module.css'
+
+const MobileBlueprintSVG = () => {
+    return (
+        <svg version="1.1" id="Layer_2_00000073002085762403623440000016108116929596944283_"
+            xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 291.9 404.1"
+            style={{ enableBackground: "new 0 0 291.9 404.1" }} xmlSpace="preserve">
+            
+            <g id="design_effects">
+                <path className={styles.st0} d="M82.5,31.2h123.9c17,0,30.8,13.8,30.8,30.8v276.9c0,17-13.8,30.8-30.8,30.8H82.5c-17,0-30.8-13.8-30.8-30.8V62
+            C51.7,45,65.5,31.2,82.5,31.2z"/>
+                <path className={styles.st1} d="M84.5,33.2h120.1c17,0,30.8,13.8,30.8,30.8v272.1c0,17-13.8,30.8-30.8,30.8H84.5c-17,0-30.8-13.8-30.8-30.8V64
+            C53.7,47,67.5,33.2,84.5,33.2z"/>
+                <path className={styles.st2} d="M113,33.2h63c0.6,0,1,0.5,1,1V40c0,3.3-2.7,6-6,6h-53.1c-3.3,0-6-2.7-6-6v-5.7C112,33.7,112.4,33.2,113,33.2
+            L113,33.2z"/>
+                <path className={styles.st1} d="M130.3,37.8h28.4c0.6,0,1,0.4,1,1v1.5c0,0.6-0.4,1-1,1h-28.4c-0.6,0-1-0.4-1-1v-1.5
+            C129.3,38.3,129.8,37.8,130.3,37.8z"/>
+                <circle className={styles.st1} cx="167" cy="39.6" r="2.1" />
+                <path className={styles.st3} d="M237.2,121.7h2.6c0.9,0,1.7,0.8,1.7,1.7v40.5c0,0.9-0.8,1.7-1.7,1.7h-2.6V121.7L237.2,121.7L237.2,121.7z" />
+                <path className={styles.st4} d="M51.2,166h-2c-0.9,0-1.7-0.8-1.7-1.7v-26.5c0-0.9,0.8-1.7,1.7-1.7h2V166L51.2,166L51.2,166z" />
+                <path className={styles.st4} d="M51.2,128.5h-2c-0.9,0-1.7-0.8-1.7-1.7v-26.5c0-0.9,0.8-1.7,1.7-1.7h2V128.5L51.2,128.5L51.2,128.5z" />
+                <path className={styles.st5} d="M51.2,88.3h-2c-0.9,0-1.7-0.8-1.7-1.7V73.7c0-0.9,0.8-1.7,1.7-1.7h2V88.3C51.2,88.3,51.2,88.3,51.2,88.3z" />
+                <line className={styles.st6} x1="14.9" y1="45.9" x2="279.2" y2="45.9" />
+                <line className={styles.st7} x1="3" y1="31.2" x2="291.9" y2="31.2" />
+                <line className={styles.st8} x1="0" y1="200.1" x2="288.9" y2="200.1" />
+                <line className={styles.st7} x1="2.6" y1="366.9" x2="291.5" y2="366.9" />
+                <line className={styles.st7} x1="2.6" y1="369.7" x2="291.5" y2="369.7" />
+                <line className={styles.st9} x1="112" y1="62.5" x2="112" y2="0" />
+                <line className={styles.st9} x1="177" y1="62.5" x2="177" y2="0" />
+                <line className={styles.st8} x1="129.3" y1="11.3" x2="129.3" y2="388.9" />
+                <line className={styles.st8} x1="159.7" y1="11.3" x2="159.7" y2="388.9" />
+                <line className={styles.st10} x1="32.5" y1="72.3" x2="70" y2="72.3" />
+                <line className={styles.st10} x1="33" y1="88.3" x2="70.4" y2="88.3" />
+                <line className={styles.st10} x1="6" y1="98.6" x2="70" y2="98.6" />
+                <line className={styles.st10} x1="6" y1="128.5" x2="70" y2="128.5" />
+                <line className={styles.st10} x1="6" y1="136.1" x2="70" y2="136.1" />
+                <line className={styles.st10} x1="6" y1="166" x2="70" y2="166" />
+                <line className={styles.st10} x1="215.2" y1="121.2" x2="279.2" y2="121.2" />
+                <line className={styles.st10} x1="215.2" y1="165.6" x2="279.2" y2="165.6" />
+                <line className={styles.st8} x1="47.5" y1="17.2" x2="47.5" y2="394.8" />
+                <line className={styles.st8} x1="241.4" y1="26.5" x2="241.4" y2="404.1" />
+            </g>
+        </svg>
+    );
+}
+ 
+export default MobileBlueprintSVG;
