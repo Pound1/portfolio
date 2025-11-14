d8387d717dcb1c9b139e760ab338473955655908
Pound1 <50385700+Pound1@users.noreply.github.com>
2025-11-14 21:44:18 +1100

    removed dupe index.html
    and some generic updates to copy

--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -8,8 +8,8 @@ import PathIconSVG from '../../assets/PathIconSVG';
 function Home() {
   return (
     <section id="home" className={styles.content}>
-        <h1 >About</h1>
-        <p className={styles.heroParagraph}>I am a <b>digital Product Owner</b>, in ANZ bank’s technology division. I bring a wealth of delivery capability after <b>10 years of building out ANZ’s marquee banking apps</b>: for Android and iOS platforms. My strengths are leading customer-centric user experiences; prioritising workflow for delivery teams; and deploying resilient features to strict industry regulations. This allows me to provide ANZ’s apps with ergonomic & practical capabilities that drive adoption <b>(currently 1M+ downloads)</b> and success <b>(4.7 Play Store, 4.8 App Store ratings)</b>.</p>
+        <h1 >Lachlan Pound</h1>
+        <p className={styles.heroParagraph}>I'm a <b>digital Product Owner</b>, in ANZ bank’s technology division. <br></br><br></br>I bring a wealth of delivery capability after <b>10 years of building out ANZ’s marquee banking apps</b>: for Android and iOS platforms. My strengths are leading customer-centric user experiences; prioritising workflow for delivery teams; and deploying resilient features to strict industry regulations. <br></br><br></br>This allows me to provide ANZ’s apps with ergonomic & practical capabilities that drive adoption <b>(currently 1M+ downloads)</b> and success <b>(4.7 Play Store, 4.8 App Store ratings)</b>.</p>
     <div className={styles.contactButton}>
         <a href="/contact">Contact</a>
     </div>
